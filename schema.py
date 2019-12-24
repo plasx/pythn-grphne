5ffd32592529bc6dc55f23bd066e31e184764221
Daniel Rivera <plasx@live.com>
2019-12-24 16:28:47 -0500

    added user resolver with limit option

--- schema.py
+++ schema.py
@@ -1,18 +1,41 @@
 import graphene
 import json
+from datetime import datetime
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
+
 
 class Query(graphene.ObjectType):
+    users = graphene.List(User, limit=graphene.Int())
     hello = graphene.String()
+    is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return "world"
+
+    def resolve_is_admin(self, info):
+        return True
+
+    def resolve_users(self, info, limit=None):
+        return [
+            User(id="1", username="Fredrico", created_at=datetime.now()),
+            User(id="2", username="Essentia", created_at=datetime.now()),
+        ][:limit]
+
 
 
 schema = graphene.Schema(query=Query)
 result = schema.execute(
     '''
     {
-        hello
+        users {
+            id
+            username
+            createdAt
+        }
     }
     '''
 )
