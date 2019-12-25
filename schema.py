8f9993edb2380ae166879d279cf0d30d5f29589f
Daniel Rivera <plasx@live.com>
2019-12-24 20:12:26 -0500

    created user post mutation

--- schema.py
+++ schema.py
@@ -4,6 +4,11 @@ import uuid
 from datetime import datetime
 
 
+class Post(graphene.ObjectType):
+    title = graphene.String()
+    content = graphene.String()
+
+
 class User(graphene.ObjectType):
     id = graphene.ID(default_value=str(uuid.uuid4()))
     username = graphene.String()
@@ -38,25 +43,35 @@ class CreateUser(graphene.Mutation):
         user = User(username=username)
         return CreateUser(user=user)
 
+class CreatePost(graphene.Mutation):
+    post = graphene.Field(Post)
+
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    def mutate(self, info, title, content):
+        post = Post(title=title, content=content)
+        return CreatePost(post=post)
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    create_post = CreatePost.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 result = schema.execute(
     '''
-    mutation($username: String) {
-        createUser(username: $username) {
-            user{
-                id
-                username
-                createdAt
-            }
+    mutation {
+      createPost(title: "Hello", content: "World") {
+        post {
+          title
+          content
         }
+      }
     }
     ''',
-    variable_values={'username': 'Dave'}
+    variable_values={'limit': 1}
 )
 
 dictResult=dict(result.data.items())
